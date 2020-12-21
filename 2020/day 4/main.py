6827c34fb9c23fdc26706515b66f44b9c85c2d4a
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-21 17:24:34 +0100

    Solution for day 4 part 1 added

--- 2020/day 4/main.py
+++ 2020/day 4/main.py
@@ -1,6 +1,25 @@
 def main():
-	print("begin")
+	f = open("input.txt", "r")
+	i = f.read()
+	passports = i.split("\n\n")
 
+	valid = 0
+
+	for passport in passports:
+		reqFields = ["byr", "iyr", "eyr", "hgt", "ecl", "pid", "hcl"]
+
+		passport = passport.replace("\n", " ")
+		fields = passport.split(" ")
+
+		for field in fields:
+			field = field.split(":")[0]
+			if field in reqFields:
+				reqFields.remove(field)
+
+		if len(reqFields) <= 0:
+			valid += 1
+
+	print(valid)
 
 if __name__ == "__main__":
-    main()
+    main()	
