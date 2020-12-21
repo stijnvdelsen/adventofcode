f0cf0198e55a56fdfdcff1df76d445db6f172780
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-21 19:13:53 +0100

    pushing part 2 of day 4. There's still an error in my regex logica I think

--- 2020/day 4/main.py
+++ 2020/day 4/main.py
@@ -1,3 +1,5 @@
+import re
+
 def main():
 	f = open("input.txt", "r")
 	i = f.read()
@@ -8,13 +10,28 @@ def main():
 	for passport in passports:
 		reqFields = ["byr", "iyr", "eyr", "hgt", "ecl", "pid", "hcl"]
 
+		reqFields = {
+			"byr": "19[2-9][0-9]|200[0-2]",
+			"iyr": "20[1|2][0-9]",
+			"eyr": "20[2|3][0-9]",
+			"hgt": "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]",
+			"ecl": "amb|blu|brn|gry|grn|hzl|oth",
+			"pid": "[0-9]{9}",
+			"hcl": "#[a-zA-Z0-9]{6}"
+		}
+
 		passport = passport.replace("\n", " ")
 		fields = passport.split(" ")
 
 		for field in fields:
+			value = field.split(":")[1]
 			field = field.split(":")[0]
+
 			if field in reqFields:
-				reqFields.remove(field)
+				regex = reqFields[field]
+
+				if re.match(regex, value):
+					del reqFields[field]
 
 		if len(reqFields) <= 0:
 			valid += 1
