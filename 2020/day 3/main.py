22dfcd6a9f1cc3bf7c6d60e3903c274222c023c8
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-21 15:26:57 +0100

    Solution for part 2 of day 3

--- 2020/day 3/main.py
+++ 2020/day 3/main.py
@@ -1,24 +1,34 @@
 def main():
 	f = open("input.txt", "r")
 	i = f.read()
-	iParsed = i.split("\n")
+	slopeMap = i.split("\n")
 
+	trees = getTrees(slopeMap, 1, 1) * getTrees(slopeMap, 3, 1) * getTrees(slopeMap, 5, 1) * getTrees(slopeMap, 7, 1) * getTrees(slopeMap, 1, 2)
+
+	print(trees)
+
+def getTrees(slopeMap, right, down):
 	index = 0
 	trees = 0
 
-	for path in iParsed:
+	i = 0
+
+	while i < len(slopeMap):
+		path = slopeMap[i]
 		symbol = path[index]
 
 		if symbol == "#":
 			trees += 1
 
-		index += 3
+		index += right
 
 		if index >= len(path):
-			index = index - len(path)		
+			index = index - len(path)	
 
+		i += down
 
-	print(trees)
+	return trees
+
 
 if __name__ == "__main__":
     # execute only if run as a script
