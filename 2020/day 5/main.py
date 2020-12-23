a784c5d206626210459ea86224c9bd8cbc45e219
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-23 16:38:02 +0100

    Solution for part 2

--- 2020/day 5/main.py
+++ 2020/day 5/main.py
@@ -43,12 +43,39 @@ def explenation():
 		print(matrix[test[0][0]][test[0][1]])
 		print("\n\n")
 
-def main():
+def part_1():
 	f = open("input.txt", "r")
 	i = f.read()
 	b = i.split("\n")
 
 	print(max(list(map(lambda p: int("0b" + ''.join(list(map(lambda l: "0" if l == "F" or l == "L" else "1", p))).lstrip("0"), 2), b))))
+
+def main():
+	f = open("input.txt", "r")
+	i = f.read()
+	b = i.split("\n")
+
+	passes = list(map(lambda p: int("0b" + ''.join(list(map(lambda l: "0" if l == "F" or l == "L" else "1", p))).lstrip("0"), 2), b))
+
+	highestRow = 127
+	highestColumn = 7
+
+	# This is for visualizing the problem to solver it faster
+	matrix = []
+
+	for y in range(0,highestRow+1):
+		matrix.append([])
+
+		for x in range(0,highestColumn+1):
+			matrix[y].append(y*8+x)
+
+	for bPass in passes:
+		passCoord = [(index, row.index(bPass)) for index, row in enumerate(matrix) if bPass in row]
+		del matrix[passCoord[0][0]][passCoord[0][1]]
+
+	# This will print a map of the seats in the airplane
+	for m in matrix:
+		print(m)
 
 if __name__ == "__main__":	
     main()
