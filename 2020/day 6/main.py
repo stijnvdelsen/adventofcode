ac61b15c4ab97013f7f985cea477aa1e22061656
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-24 13:59:00 +0100

    Dirty solution for part 1 day 6

--- 2020/day 6/main.py
+++ 2020/day 6/main.py
@@ -1,4 +1,4 @@
-def main():
+def part_1():
 	f = open("input.txt", "r")
 	i = f.read()
 	questions = i.split("\n\n")
@@ -13,6 +13,26 @@ def main():
 			for answer in person:
 				if answer not in answers:
 					answers.append(answer)
+
+		cntr += len(answers)
+
+	print(cntr)
+
+def main():
+	f = open("input.txt", "r")
+	i = f.read()
+	questions = i.split("\n\n")
+
+	cntr = 0
+
+	for group in questions:
+		persons = group.split('\n')
+		answers = group
+
+		for letter in group:
+			for person in persons:
+				if letter not in person:
+					answers.strip(letter)
 
 		cntr += len(answers)
 
