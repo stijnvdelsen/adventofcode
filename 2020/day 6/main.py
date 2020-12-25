281ad317fbc25ae6a179b59901d907fbb579ce65
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-12-25 17:33:14 +0100

    Can't find the right solution this fast, so save my work and im going to start over...

--- 2020/day 6/main.py
+++ 2020/day 6/main.py
@@ -21,34 +21,9 @@ def part_1():
 def main():
 	f = open("input.txt", "r")
 	i = f.read()
-	questions = i.split("\n\n")
+	d = i.split("\n\n")
 
-	cntr = 0
-
-	for group in questions:
-		persons = group.split('\n')
-		answers = list(set(group.replace('\n', '')))
-
-		# print(answers)
-		# print(group)
-
-		for person in persons:
-			for letter in group.replace('\n', ''):
-				if letter not in person and letter in answers:
-					answers.remove(letter)
-
-		# for letter in group.replace('\n', ''):
-		# 	for person in persons:
-		# 		if letter not in person:
-		# 			print(letter)
-		# 			answers.remove(letter)
-
-		cntr += len(answers)
-
-	print(cntr)
+	
 
 if __name__ == "__main__":	
     main()
-
-
- # Alle letters die persoon bevat
